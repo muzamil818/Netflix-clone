c8f51b561506ed810cdfa6d0e3f9a2b2167cc19e
muzamil al <muzamilali.dev@gmail.com>
2025-05-09 01:56:34 +0500

    added api and onclick functionality on every movie card

--- src/components/TitleCard/TitleCard.jsx
+++ src/components/TitleCard/TitleCard.jsx
@@ -1,53 +1,61 @@
-import React, { useEffect, useRef, useState} from 'react'
-import './TitleCard.css'
+import React, { useEffect, useRef, useState } from "react";
+import "./TitleCard.css";
+import { Link, } from "react-router-dom";
 // import card_data from '../../assets/cards/Cards_data'
 
-const TitleCard = ({title, catagory}) => {
-  const [apiData, setApiData] = useState([])
-    const cardsRef = useRef();
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjQ2NmFjNjQzNTE3NzA3ZGQxYTUzNWUxMzdjNmI2YSIsIm5iZiI6MS43NDY2MTUzMTA4MTQwMDAxZSs5LCJzdWIiOiI2ODFiM2MwZWUyNjVhOWIwYmZjNjk5N2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.trP_idLEj31QNn-BL9Q9wtsG_Zo3aGYNqmFPOj6DHUc'
-      }
-    };
-    
-  
-    const handleWheel = (event) => {
-      event.preventDefault();
-      const scrollAmount = event.deltaY * 0.90; // Adjust the factor if needed
-      cardsRef.current.scrollLeft += scrollAmount;
-    };
-  
-    useEffect(() => {
-       
-    fetch(`https://api.themoviedb.org/3/movie/${catagory?catagory:"now_playing"}?language=en-US&page=1`, options)
-    .then(res => res.json())
-    .then(res => setApiData(res.results))
-    .catch(err => console.error(err));
-      const current = cardsRef.current;
-      if (current) {
-        current.addEventListener('wheel', handleWheel);
-      }
-  
-    }, []);
-  
+const TitleCard = ({ title, catagory }) => {
+
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjQ2NmFjNjQzNTE3NzA3ZGQxYTUzNWUxMzdjNmI2YSIsIm5iZiI6MS43NDY2MTUzMTA4MTQwMDAxZSs5LCJzdWIiOiI2ODFiM2MwZWUyNjVhOWIwYmZjNjk5N2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.trP_idLEj31QNn-BL9Q9wtsG_Zo3aGYNqmFPOj6DHUc",
+    },
+  };
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    const scrollAmount = event.deltaY * 0.9; // Adjust the factor if needed
+    cardsRef.current.scrollLeft += scrollAmount;
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        catagory ? catagory : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+    const current = cardsRef.current;
+    if (current) {
+      current.addEventListener("wheel", handleWheel);
+    }
+  }, []);
 
   return (
-    <div className='title-card'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
-      <div className='card-list' ref={cardsRef}>
-      {apiData.map((card, index)=>{
-          return <div className='card' key={index} >
-            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt='movie'/>
-            <p>{card.original_title}</p>
-          </div>
-      })}
-
-</div>
+    <div className="title-card">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {apiData.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img 
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt="movie"  
+              />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCard
+export default TitleCard;
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Player.css"
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 const Player = () => {
+  const {id} = useParams()
+  const navigate = useNavigate();
+  const [apiData, setApiData] = useState({
+    name:"",
+    key:"",
+    published_at:"",
+    type:""
+
+  })
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjQ2NmFjNjQzNTE3NzA3ZGQxYTUzNWUxMzdjNmI2YSIsIm5iZiI6MS43NDY2MTUzMTA4MTQwMDAxZSs5LCJzdWIiOiI2ODFiM2MwZWUyNjVhOWIwYmZjNjk5N2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.trP_idLEj31QNn-BL9Q9wtsG_Zo3aGYNqmFPOj6DHUc'
+    }
+  };
+  useEffect(()=>{
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results[0]))
+      .catch(err => console.error(err));
+  },[])
   return (
     <div className='player' >
-      <img src={back_arrow_icon} alt='backArrow' className='backArrow'/>
-      <iframe className='iframe' src='https://www.youtube.com/embed/pAsmrKyMqaA' title='trailer' width='90%' height="90%" frameBorder='0'></iframe>
+      <img  src={back_arrow_icon} alt='backArrow' className='backArrow' onClick={()=>{navigate(-2)}}/>
+      <iframe className='iframe' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' width='90%' height="90%" frameBorder='0'></iframe>
       <div className='player-info'>
-        <p>Publish Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   )
